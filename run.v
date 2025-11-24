03e71708eb131f185f636c0e640c10a57295d2de
Mandeep Singh <135956602+MannCode@users.noreply.github.com>
2025-11-23 22:11:44 -0330

    added run.v for backup

---
+++ run.v
@@ -0,0 +1,44 @@
+// Read Me: This V script performs platform-dependent execution of build and run commands
+// Copy this file and put it in the build/ directory as run.v to use it.
+// goto build/ in your terminal and run `v run run.v`
+
+
+import os
+
+fn platform_dependent_execution() {
+    mut commands := []string{}
+
+    if os.user_os() == 'macos' {
+        commands = [
+            'find . ! -name run.v -delete',
+            'cmake ..',
+            'make -j8',
+            './NN',
+        ]
+    } else if os.user_os() == 'windows' {
+        commands = [
+            'for /d %i in (*) do if not "%i"=="_deps" rmdir /s /q "%i" & for %i in (*) do if not "%i"=="run.v" del /q "%i"',
+            'cmake ..',
+            'cmake --build . --config Release',
+            'cd Release && NN.exe',
+        ]
+    } else {
+        eprintln('Running on an unsupported OS: ${os.user_os()}')
+        return
+    }
+
+    for cmd in commands {
+        println('Running: $cmd')
+        result := os.execute(cmd)
+        if result.exit_code != 0 {
+            eprintln('Command failed: $cmd')
+            eprintln('Error output: $result.output')
+            exit(result.exit_code)
+        }
+    }
+}
+
+fn main() {
+    println('Starting platform-dependent execution...')
+    platform_dependent_execution()
+}
